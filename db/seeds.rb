da859ad7dc19c3ce11d4d750ab60b42e2392db68
Matt Readout <matt.readout@gmail.com>
2019-01-04 10:21:25 -0500

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,35 @@ industrial = Soundscape.create({
   theme: 'Mechanical'
   })
 
+nature = Soundscape.create({
+  name: 'Nature Sounds',
+  theme: 'Natural'
+  })
+
+night = Soundscape.create({
+  name: 'Night Ambiance',
+  theme: 'Calming'
+  })
+
+crowd_city = Soundscape.create({
+  name: 'City Noise',
+  theme: 'Frenetic'
+  })
+
 Sound.create([
   { name: 'Waves', source: 'maine_beach.m4a', description: 'Waves periodically washing over rocks', soundscape: beach_test },
   { name: 'Stream', source: 'water_flowing.wav', description: 'Water flowing in stream', soundscape: beach_test },
   { name: 'Steamboat', source: 'steamboat.wav', description: 'Steamboat horn', soundscape: beach_test},
   { name: 'Bodyscanner', source: 'mri_bodyscan.wav', description: 'MRI Bodyscanner', soundscape: industrial},
   { name: 'Girders', source: 'metal_clangs.wav', description: 'Girders clanging', soundscape: industrial},
-  { name: 'Presser', source: 'trouser_press.wav', description: 'Trouser press operating', soundscape: industrial}
+  { name: 'Presser', source: 'trouser_press.wav', description: 'Trouser press operating', soundscape: industrial},
+  { name: 'Bugs', source: 'bugs_by_the_lake.m4a', description: 'Insects buzzing by a lake', soundscape: nature},
+  { name: 'Birds', source: 'birds_chirping.m4a', description: 'Birds chirping', soundscape: nature},
+  { name: 'Pond', source: 'pond_at_bbg.m4a', description: 'Pond with a small waterfall', soundscape: nature},
+  { name: 'Frogs', source: 'hillbrook_11pm.m4a', description: 'Insects and frogs at night', soundscape: night},
+  { name: 'Fire', source: 'poblanos_toasting.m4a', description: 'Small fire crackling', soundscape: night},
+  { name: 'Crickets', source: 'storm_king.m4a', description: 'Crickets chirping', soundscape: night},
+  { name: 'Park', source: 'park_time.m4a', description: 'Music and voices in a city park', soundscape: crowd_city},
+  { name: 'Train', source: 'bbp_at_night.m4a', description: 'Train passing overhead', soundscape: crowd_city},
+  { name: 'Beer Hall', source: 'beer_hall_greenpoint.wav', description: 'Voices in crowded bar', soundscape: crowd_city}
   ])
